fbe490b2f5ac0a84b839ba712c073099cbe2ef83
elbinhilongo <elbinhilongo@gmail.com>
2024-05-16 13:50:57 +0800

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+/*****COMMON CSS SECTION*****/
+* {
+	box-sizing: border-box;
+	margin: 0;
+	padding: 0;
+	-outline: 0;
+	font-family Roboto, sans-serif, arial;
+}
+html, body 
+	{
+	font-size: 14px;
+	font-weight: 400;
+	background: #000000;
+	color: #fff;
+}
+.container {
+	width: 1100px;
+	margin: 0px auto;
+	display: table;
+}
+.width-33 {
+	width: 33%;
+	float: left;
+}
+.width-66 {
+	width: 66%;
+	float: left;
+}
